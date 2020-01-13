3f39884ddd73cb9f06ed301c96bc5ba45066bbd0
lupyuen <luppy@appkaki.com>
2020-01-14 04:40:01 +0800

    Test

--- src/jtag/drivers/spi.c
+++ src/jtag/drivers/spi.c
@@ -70,9 +70,12 @@ static unsigned int lsb_buf_bit_index;
 /// File descriptor for SPI device
 static int spi_fd = -1;
 
-//  SWD Sequence to Read Register 0 (IDCODE), prepadded with 2 null bits bits to fill up 6 bytes. Target will not get out of sync after sequence.
+/// SWD Sequence to Read Register 0 (IDCODE), prepadded with 2 null bits bits to fill up 6 bytes. Target will not get out of sync after sequence.
 static const uint8_t swd_read_reg_0_prepadded[] = { 0x94, 0x02, 0x00, 0x00, 0x00, 0x00 };
 static const unsigned swd_read_reg_0_prepadded_len = 48;  //  Number of bits
+
+/// SWD Sequence for null byte, used by bitbang_swd_run_queue()
+static const uint8_t null_byte[0] = { 0 };
 
 static void spi_exchange_transmit(uint8_t buf[], unsigned int offset, unsigned int bit_cnt);
 static void spi_exchange_receive(uint8_t buf[], unsigned int offset, unsigned int bit_cnt);
@@ -93,7 +96,6 @@ void spi_exchange(bool target_to_host, uint8_t buf[], unsigned int offset, unsig
 {
     if (!buf && bit_cnt == 8) {
         // bitbang_swd_run_queue() calls bitbang_exchange() with buf=NULL and bit_cnt=8. We send a null byte.
-        static const uint8_t null_byte[0] = { 0 };     
         buf = null_byte;   
     }
     if (!buf) { pabort("spi_exchange: null buffer"); return; }
